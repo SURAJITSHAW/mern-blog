ece5f4f1c86969f8b59d2a31fca6a95c4131eed4
SURAJITSHAW <piklu3043@gmail.com>
2024-04-17 18:59:54 +0530

    db connection established

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
 
 const app = express();
 
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB connection established");
+    })
+    .catch((err) => {
+        console.error("Error connecting to database:", err);
+    });
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+    console.log("Server is running on port 3000");
+});
