3571eebe847a23531f1305f83ab8f6539cba75cd
SURAJITSHAW <piklu3043@gmail.com>
2024-04-18 01:30:38 +0530

    sign-in endpoint done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorhandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -14,10 +15,10 @@ const signup = async (req, res, next) => {
         email === ""
     ) {
         // ! here we actually dont have an error object, but have a logical error
-        return res.status(400).json({
-            message: "Please fill all the fields",
-        });
-        // next(errorhandler(400, "Please fill all the fields"));
+        // return res.status(400).json({
+        //     message: "Please fill all the fields",
+        // });
+        next(errorhandler(400, "Please fill all the fields"));
     }
 
     const hashPassword = bcrypt.hashSync(password, 10);
@@ -41,4 +42,50 @@ const signup = async (req, res, next) => {
     }
 };
 
-export { signup };
+const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (
+        !password ||
+        !email ||
+        password === "" ||
+        email === ""
+    ) {
+        // ! here we actually dont have an error object, but have a custom error
+        // return res.status(400).json({
+        //     message: "Please fill all the fields",
+        // });
+        next(errorhandler(400, "Please fill all the fields"));
+    }
+try {
+    
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+        next(errorhandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+        next(errorhandler(400, "Invalid password"));
+    }
+
+    const token = jwt.sign(
+        { id: validUser._id, username: validUser.username },
+        process.env.JWT_SECRET
+    );
+
+    const { password: userPassword, ...userWithoutPassword } = validUser._doc;
+
+    res.status(200)
+        .cookie("access_token", token, {
+            httpOnly: true,
+        })
+        .json({
+            success: true,
+            data: userWithoutPassword,
+        });
+} catch (error) {
+    next(error);
+}
+
+}
+
+export { signup, signin };
