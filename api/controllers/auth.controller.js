e11115f561ae2a05639818c21b8492f3119b9208
SURAJITSHAW <piklu3043@gmail.com>
2024-04-17 22:04:46 +0530

    create a sign-in end point

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
-import User from "../models/user.model.js"
-import bcrypt from "bcryptjs"
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { errorhandler } from "../utils/error.js";
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (
@@ -12,29 +13,31 @@ const signup = async (req, res) => {
         password === "" ||
         email === ""
     ) {
+        // ! here we actually dont have an error object, but have a logical error
         return res.status(400).json({
             message: "Please fill all the fields",
         });
+        // next(errorhandler(400, "Please fill all the fields"));
     }
 
     const hashPassword = bcrypt.hashSync(password, 10);
-
     const newUser = new User({
         username,
         email,
-        password: hashPassword
-    })
+        password: hashPassword,
+    });
 
     try {
         const user = await newUser.save();
         res.status(201).json({
             message: "User created successfully",
-            user
-        })
+            user,
+        });
     } catch (error) {
-        res.status(400).json({
-            message: error.message,
-        })
+        // res.status(400).json({
+        //     message: error.message,
+        // })
+        next(error);
     }
 };
 
--- api/index.js
+++ api/index.js
@@ -5,10 +5,25 @@ import "dotenv/config";
 import authRouter from "./routes/auth.route.js";
 
 
+
 const app = express();
 
-// ! middlewares
+
+// ! middleware
 app.use(express.json());
+// ! routes
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+// ! error handler middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+})
 
 mongoose
     .connect(process.env.MONGODB_URI, {
@@ -23,9 +38,7 @@ mongoose
     });
 
 
-// ! routes
-app.use("/api/users", userRouter);
-app.use("/api/auth", authRouter)
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
